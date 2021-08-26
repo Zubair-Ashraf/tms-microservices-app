de4b39b4d7bbf08ab62bd8b1d8c5867069d3dc1c
Zubair-Ashraf <zubair8767844@gmail.com>
2021-08-26 15:35:52 +0500

    auth service setup and install dependencies

---
+++ services/auth-service/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () =>
+  console.log(`Auth service is listening at port: ${port}`)
+);
