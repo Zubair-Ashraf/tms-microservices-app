92e4e6c11706964ddfd5761bcf27747b7e09e214
Zubair-Ashraf <zubair8767844@gmail.com>
2021-09-15 12:17:08 +0500

    nats-streaming event subject and data typo

--- nats-streaming/src/events/create-ticket-listener.ts
+++ nats-streaming/src/events/create-ticket-listener.ts
@@ -1,12 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './events-types';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
 
   queueGroupName = 'tickets-service';
 
-  onMessage(data: any, message: Message) {
+  onMessage(data: TicketCreatedEvent['data'], message: Message) {
     console.log(data);
 
     message.ack();
