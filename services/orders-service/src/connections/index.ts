383d3c3c5fbe6f6abd74cbdb552b01c16fad662c
Zubair-Ashraf <zubair8767844@gmail.com>
2021-09-16 11:48:27 +0500

    orders-service setup

---
+++ services/orders-service/src/connections/index.ts
@@ -0,0 +1,2 @@
+export * from './database-connection';
+export * from './nats-connection';
---
+++ services/orders-service/src/index.ts
@@ -0,0 +1,62 @@
+import express, { Request } from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+// import * as routes from './routes';
+import * as middlewares from '@zkode/tms-lib';
+import { NotFoundError } from '@zkode/tms-lib';
+import { DatabaseConnection, NatsConnection } from './connections';
+import { natsClient } from './clients';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.set('trust proxy', true);
+
+app.use(cookieSession({ secure: false, signed: false }));
+
+app.use('/api/tickets', [
+  // routes.GetTicketRouter,
+  // routes.CreateTicketRouter,
+  // routes.UpdateTicketRouter,
+]);
+
+app.all('*', (req: Request) => {
+  throw new NotFoundError(req.path);
+});
+
+app.use(middlewares.errorhandler);
+
+const launchApp = () => {
+  app.listen(port, () =>
+    console.log(`Ticket service is listening at port: ${port}`)
+  );
+};
+
+const startup = () => {
+  DatabaseConnection()
+    .then(() => console.log('Database connected!'))
+    .catch((err) => console.log('Database connection failed: ', err));
+
+  NatsConnection()
+    .then(() => {
+      console.log('NATS server connected');
+
+      natsClient.client.on('close', (err) => {
+        console.log('NATS server connection closed', err);
+
+        process.exit();
+      });
+
+      process.on('SIGINT', () => natsClient.client.close());
+
+      process.on('SIGTERM', () => natsClient.client.close());
+
+      launchApp();
+    })
+    .catch((err) => console.log('Nats connection failed: ', err));
+};
+
+startup();
